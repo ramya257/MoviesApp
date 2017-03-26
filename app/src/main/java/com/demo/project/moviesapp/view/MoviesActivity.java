2779e2f87e40bcbfa05948fee1f4aeb815341769
ramya <educate1996@gmail.com>
2017-03-26 16:49:58 +0530

    removed unwanted menu folder

--- app/src/main/java/com/demo/project/moviesapp/view/MoviesActivity.java
+++ app/src/main/java/com/demo/project/moviesapp/view/MoviesActivity.java
@@ -30,7 +30,7 @@ public class MoviesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_movies);
-//        toolbar.setTitle("MoviesApp");
+        toolbar.setTitle("MoviesApp");
         toolbar=(Toolbar)findViewById(R.id.toolbar);
         toolbar.setTitleTextColor(ContextCompat.getColor(this, R.color.white));
         setSupportActionBar(toolbar);
